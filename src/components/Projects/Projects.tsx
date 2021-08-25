2c57a3a9c50f5330141363c93868f1962867ac14
jgiwinski <juliaiwinski@gmail.com>
2021-08-25 14:23:10 -0700

    Add project data to repo

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,10 +1,24 @@
 import './Projects.scss'; 
+import { Project } from '../Project/Project';
+import { projectData } from '../../data/projectData'; 
 
 export const Projects = () => {
+
+    const allProjects = projectData.map(project => {
+        return <Project 
+        name = {project.name}
+        github =  {project.github}
+        deploy = {project.deploy} 
+        desc = {project.desc}
+        stack = {project.stack}
+        media = {project.media}
+    />
+})
+
     return (
         <section id='projects' >
-            <h1>PROJECTS PAGE</h1>
-            <section className='box'></section>
+            <h2>PROJECTS</h2>
+            {allProjects}
         </section>
     )
 }
