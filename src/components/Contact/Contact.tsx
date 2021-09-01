6c4abfde6033968ed7c06db97ced546a4d729c34
jgiwinski <juliaiwinski@gmail.com>
2021-09-01 14:33:27 -0700

    Add contact icons to DOM

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,10 +1,18 @@
 import './Contact.scss'; 
+import { FaLinkedin, FaGithubSquare, FaEnvelope } from "react-icons/fa";
 
 export const Contact = () => {
     return (
         <section id='contact'>
-            <h1>CONTACT PAGE</h1>
-            <section className='box'></section>
+            <h1 className='section-title'>CONTACT ME</h1>
+            <hr></hr>
+            <img ></img>
+            <h3>Let's connect!</h3>
+            <div>
+                <FaLinkedin onClick={() => window.open("https://www.linkedin.com/in/julia-iwinski-898540138/")}/>
+                <FaGithubSquare onClick={() => window.open("https://github.com/jgiwinski/")}/>
+                <FaEnvelope onClick={() => window.open("https://www.juliaiwinski@gmail.com")}/>
+            </div>
         </section>
     )
 }
--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -18,14 +18,14 @@ export const Project = ({ name, github, deploy, desc, stack, media }: project ) 
                 <section className='project-desc'>
                     <p className='bottom-space'>{desc}</p>
                     <div className='tech-card'>
-                        <h5>TECH STACK</h5>
+                        <h4>TECH STACK:</h4>
                         <div className='tech-stack'>
                         {stack.map(name => <p>{name}</p>)}
                         </div>
                     </div>
                 </section>
                 <section>
-                    <img src={media} className='project-media'/>
+                    <img src={media} className='project-media' alt={name}/>
                     <div className='project-links'>
                         <a href={github} className='code-link'>CODE</a>
                         <a href={deploy} className='deploy-link'>DEPLOYED</a>
